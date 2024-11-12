a95b89e7b605667c99ab963b4bc48774892eb12f
GalaxionZero <zetayu24@gmail.com>
2024-11-12 10:56:39 +0800

    pushing to checkout another branch

--- laptop.cpp
+++ laptop.cpp
@@ -12,7 +12,6 @@ struct Laptop {
     int id;
     string brand;
     string model;
-    int stock;
 };
 
 struct Accounts {
@@ -105,70 +104,17 @@ void menuMain() {
     }
 }
 
-void listLaptop(Laptops *head){
-   if(head == nullptr){
-         cout<<"List Laptop kosong"<<endl;
-   }else{ 
-        int no = 1;
-        cout << "No\tID\tBrand\tModel\tStok" << endl;
-            while(head != nullptr){
-                cout << no << "\t" << head->data.id << "\t" << head->data.brand << "\t" << head->data.model << "\t"<< head->data.stock <<endl;
-                no++;
-                head = head->next;
-            }
-        cout<<endl;
-    }
-}
-
-Laptops inputData(int &lastId){
-    Laptops *laptopBaru = new Laptops();
-    laptopBaru->data.id = ++lastId;
-    cout << "Brand: ";
-    cin.ignore(); // to ignore the newline character left by cin
-    getline(cin, laptopBaru->data.brand);
-    cout << "Model: ";
-    getline(cin, laptopBaru->data.model);
-    cout << "Stok: ";
-    cin >> laptopBaru->data.stock;
-    return *laptopBaru;
-}
-
-void addLaptopFirst(Laptops *&head, int &jumlahlaptop, int &lastId){
-    Laptops *laptopBaru = new Laptops();
-    laptopBaru->data = inputData(lastId).data;
-    laptopBaru->next = head;
-    head = laptopBaru;
-    jumlahlaptop++;
-}
-
-void addLaptop(Laptops *&head, int &jumlahlaptop, int &lastId){
-    if(head == nullptr){
-        addLaptopFirst(head, jumlahlaptop, lastId);
-        return;
-    }
-    Laptops *laptopBaru = new Laptops();
-    laptopBaru->data = inputData(lastId).data;
-    laptopBaru->next = nullptr;
-
-    Laptops *temp = head;
-    while(temp->next != nullptr){
-        temp = temp->next;
-    }
-    temp->next = laptopBaru;
-    jumlahlaptop++;
-}
-
-void menuAdmin(Laptops *head, int &jumlahLaptop, int &lastId) {
+void menuAdmin() {
     string opsi[] = {"List Laptop", "Tambah Laptop", "Ubah Laptop", "Hapus Laptop", "Logout"};
     string opsi_header = "Menu Admin";
     int jumlah_opsi = 5;
     int pilih = showmenu(jumlah_opsi, opsi, opsi_header);
     switch (pilih) {
         case 0:
-            listLaptop(head);
+            // listLaptop();
             break;
         case 1:
-            addLaptop(head, jumlahLaptop, lastId);
+            // tambahLaptop();
             break;
         case 2:
             // ubahLaptop();
@@ -204,13 +150,6 @@ void menuUser() {
 }
 
 int main() {
-    int jumlahLaptop = 0, jumlahAccount = 1;
-    int lastId = 0;
-    Laptops *head = nullptr;
-    Accounts *headakun = nullptr;
-    addLaptop(head, jumlahLaptop, lastId);
-    listLaptop(head);
-    _getch();
-    // menuMain();
+    menuMain();
     return 0;
 }
